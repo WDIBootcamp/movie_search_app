695819f62dd90607aaba5ac666ce7ccacc8845fd
ste claudino daffara <ste.daff@gmail.com>
2014-01-16 20:56:05 -0800

    first commit

---
+++ omdb.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'typhoeus'
+require 'json'
+
+get '/' do
+  
+  erb :index
+  #display movies
+end
+
+# the CREATE METHOD
+post '/results' do
+  # the movie selected
+  @search_str = params[:movie]
+
+  # get movies from omdb api
+  response = Typhoeus.get("www.omdbapi.com", :params => {:s => @search_str})
+  # parsing(making it look nice) -- the hash
+  result = JSON.parse(response.body)
+  # sorting the results
+  @sorted_result = result["Search"].sort_by{ |movie| movie['Year'] } 
+
+  erb :results
+
+end
+
+# the SHOW METHOD/ROUTE for movie
+get '/movies/:imdb' do |imdb_id|
+
+  response2 = Typhoeus.get("www.omdbapi.com", :params => {:i => imdb_id})
+  @result2 = JSON.parse(response2.body)
+
+  erb :movie
+  
+end
+
+
+get '/imdbid/:imdb' do |imdb_id|
+
+  # reroute to imdb's website
+  redirect "http://www.imdb.com/title/#{imdb_id}"
+
+end
+
